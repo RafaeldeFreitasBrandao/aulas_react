60fbac730518c6d730f17974ca98fdf1e7240c78
Rafael de Freitas Brandão <rafael.f.brandao2006@gmail.com>
2026-07-10 11:37:23 -0300

    learing_motion

---
+++ motin-learning/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import {motion} from "motion/react";
+
+function App() {
+  
+
+  return (
+    <>
+     <motion.div>
+        <h1>Hello World</h1>
+     </motion.div>
+    </>
+  )
+}
+
+export default App

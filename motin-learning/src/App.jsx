6d4b2be311ded1235171640f39f08a552984c538
Rafael de Freitas Brandão <rafael.f.brandao2006@gmail.com>
2026-07-10 11:55:11 -0300

    basic use of motion
    Changing the oppacity of the element

--- motin-learning/src/App.jsx
+++ motin-learning/src/App.jsx
@@ -1,15 +1,18 @@
 
-import './App.css'
-import {motion} from "motion/react";
+import './App.css';
+import {motion} from "motion/react"
 
 function App() {
-  
 
   return (
     <>
-     <motion.div>
-        <h1>Hello World</h1>
-     </motion.div>
+    <motion.div initial={{opacity: 0 }} 
+                animate={{opacity: 1}}
+                transition={{duration: 0.8}}
+    >
+
+      <h1>Hello World!</h1>
+    </motion.div>
     </>
   )
 }
